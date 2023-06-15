9c1be21ac55cd16ddae7c16eb5f2f2dc515b73aa
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-15 12:38:23 -0700

    C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 int main(void)
 /* Printing using the puts function*/
 {
-    char Mystr[] = "\"Programming is like building a multilingual puzzle";
-    puts(Mystr);
-
-    return 0;	
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return(0);
 }
