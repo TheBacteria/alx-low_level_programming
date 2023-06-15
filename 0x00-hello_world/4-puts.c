b821931ab5813035505ed4349b08cfa332251786
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-15 12:45:32 -0700

    C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
-/* Printing using the puts function*/
 {
-char Mystr[] = "\"Programming is like building a multilingual puzzle";
-puts(Mystr);
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
