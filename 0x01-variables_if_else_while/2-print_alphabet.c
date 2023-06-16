021bbda7232011b4d5f0e98b5c25929f92eaca94
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-16 09:31:32 -0700

    C language

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; ++c)
+		putchar(c)
+	return (0);
+}
