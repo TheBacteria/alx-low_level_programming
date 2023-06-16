f9743fa29c0c70de2a37f9080b53311481df6ab4
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-16 11:10:16 -0700

    C language

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers from 0 to 10
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9 ; n++)
+		printf("%d", n);
+	return (0);
+}
