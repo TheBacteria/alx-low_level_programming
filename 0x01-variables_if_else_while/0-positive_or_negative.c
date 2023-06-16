5b96b8050d2aab4f4b08939019d71872ecedb9ed
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-16 08:39:52 -0700

    C language

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,13 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: Positive or negative detector
+ *
+ * Return: Always 0 (success)
+*/
 int main(void)
 {
 		int n;
@@ -10,11 +17,11 @@ int main(void)
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
 					/* your code goes there */
-				        if (n > 0)
-						printf("%d is positive",n);
-					else if (n < 0)
-						printf("%d is negative",n);
-					else
-						printf("%d is zero",n);
-					return (0);
+		if (n > 0)
+			printf("%d is positive", n);
+		else if (n < 0)
+			printf("%d is negative", n);
+		else
+			printf("%d is zero", n);
+		return (0);
 }
