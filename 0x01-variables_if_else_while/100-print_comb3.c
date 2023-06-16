6c85cd318aefc92d392a4db818bbc157bfc3b654
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-16 15:45:50 -0700

    C language

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing combinations of 2 digits
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n1 = 0;
+	int n2;
+
+	while (n1 <= 9)
+	{
+		putchar(n1 + '0');
+		n2 = n1++;
+		while (n2 <= 9)
+		{
+			putchar(n2 + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
