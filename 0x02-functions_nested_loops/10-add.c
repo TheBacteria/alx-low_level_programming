990fbc74a62234f87efb4becf135b9b35bf2426f
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-20 06:56:37 -0700

    C language

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - a function that adds 2 ints
+ *
+ * @a: the first input
+ * @b: the second input
+ *
+ * Return: a+b
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
+
