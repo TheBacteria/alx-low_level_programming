c327a61afd3646515c4eb46a4f23841532855988
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-20 05:48:03 -0700

    C language

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute
+ *        value of an integer
+ *
+ * @n: takes the int input
+ *
+ * Return: Always 0 (Success)
+*/
+int _abs(int n)
+{
+	if (n <0)
+		n = (-1) * n;
+	return (n):
+}
