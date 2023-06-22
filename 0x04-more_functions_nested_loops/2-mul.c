aab50c0ee6ea80319f2e70464eaece13dd6f893f
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-22 10:35:10 -0700

    C language

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 ints
+ *
+ * @a: takes input of the first number
+ * @b: takes input of the second number
+ *
+ * Return: the result
+*/
+int mul(int a, int b)
+{
+	result = a * b;
+	return (result);
+}
