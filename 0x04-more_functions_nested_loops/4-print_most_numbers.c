efc584d5182f6d3745029a2634f7e5609693cbe5
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-22 14:08:27 -0700

    C language

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,13 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int n;
 
-	for (c = '0'; c <= 9; c++)
+	for (n = 0; n <= 9; n++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n + 48);
 	}
 	_putchar('\n');
 }
