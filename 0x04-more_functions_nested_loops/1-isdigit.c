4fb13ce8835016f793aa481bf3cc355c7f5a6f54
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-22 10:29:22 -0700

    C language

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - checks if c is a number
+ *
+ * @c: takes input
+ *
+ * Return: 1 if it's a number, otherwise 0
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && <= 57)
+		return (1);
+	else
+		return (0);
+}
