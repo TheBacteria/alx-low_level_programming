cdf6d5dd52f99e36a6dfff41590ce60e46593f63
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-22 11:33:06 -0700

    C language

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,15 @@
 /**
  * print_numbers - prints  from 0 to 9
  *
+ * Return: Always 0 (Success)
 */
 void print_numbers(void)
 {
 	int c;
 
 	for (c = 0; c <= 9; c++)
+	{
 		_putchar(c + 48);
+	}
 	_putchar('\n');
 }
