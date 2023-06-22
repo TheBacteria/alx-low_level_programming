c85c50303b1d0acc07ec5d857de7038a34bc538d
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-22 10:17:15 -0700

    C language

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * _isupper - check if c is upper or lower
+ *
+ * @c: takes the input
+ *
+ * Return: 1 if c is upper, 0 if c is lower
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
