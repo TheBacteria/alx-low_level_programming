c873435b1f33af342c951f4e024444a382fd7bcb
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-22 15:31:37 -0700

    C language

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,17 @@
 void print_diagonal(int n)
 {
 	int i;
+	int space;
 
 	if (n > 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			if (i < n)
+			for (space = 0; space <= n && space != 0; space++)
 				_putchar(' ');
-			else
-				_putchar('\\');
+			_putchar('\\');
+			if (i != n)
+				_putchar('\n');
 		}
 	}
 	_putchar('\n');
