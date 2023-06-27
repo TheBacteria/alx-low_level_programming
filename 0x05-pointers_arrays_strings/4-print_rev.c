72513d16e309e2ecc1d37e2e9c2275a53fdbe68a
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-26 18:11:40 -0700

    C language

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: the string to print
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+	{
+		_putchar([i]);
+	}
+	_putchar('\n');
+}
