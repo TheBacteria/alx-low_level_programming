e868afaaa0a1db520fd48ce0bf33ed9310ca8a52
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-29 15:46:45 -0700

    C language

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,15 +6,22 @@
  * @s1: A pointer to the first string to be compared
  * @s2: A pointer to the second string to be compared
  * 
- * Return: the difference
+ * Return: 1 if true, 0 if false
 */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int sum = 0;
+
+	while (*s1)
 	{
+		if (*s1 != *s2)
+		{
+			sum = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	return (sum);
 }
