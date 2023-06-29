b2eb26bfe05dff4b1579f9ce705a4d9e0fb5ed62
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-29 15:09:42 -0700

    C language

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/***/
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest [i])
+		i++;
+	for (j = 0; src[j]; j++)
+		dest[i] = src[j]
+
+	return (dest);
+}
