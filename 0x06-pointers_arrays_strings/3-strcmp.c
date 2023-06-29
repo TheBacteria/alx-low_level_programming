ec801770a042f5b1c381ac703c78f1cc177f05d8
Bacteria <mahmoud1.zouine2@gmail.com>
2023-06-29 15:41:15 -0700

    C language

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strcmp - compares pointers to two strings
+ * 
+ * @s1: A pointer to the first string to be compared
+ * @s2: A pointer to the second string to be compared
+ * 
+ * Return: the difference
 */
-int main(void)
-{
-	char s1[] = "Hello";
-	char s2[] = "World!";
 
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
-	return (0);
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
