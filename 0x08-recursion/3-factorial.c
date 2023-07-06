98db834c6a4977a52834a5782ee510ee8fa25720
Bacteria <mahmoud1.zouine2@gmail.com>
2023-07-05 17:14:54 -0700

    C language

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - calculates the factorial of n
+ *
+ * @n: the input number
+ *
+ * Return: factorial
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
