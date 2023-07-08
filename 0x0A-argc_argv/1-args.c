78a505ee7217b0800de643bc7e6871566faff5e1
Bacteria <mahmoud1.zouine2@gmail.com>
2023-07-07 18:15:47 -0700

    C language

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: number of arguments
+ * @argv: pointer to an array
+ *
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
