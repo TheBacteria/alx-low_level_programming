a324e2e9a4d0ee0a2df0e92404606523081462b4
Bacteria <mahmoud1.zouine2@gmail.com>
2023-07-10 17:06:17 -0700

    C language

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,18 +14,18 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	
-	while (str[size] != '\0')
-		size++;
-	
+
+	for (; str[size] != '\0'; size++)
+	;
+
 	a = malloc(size * sizeof(*str) + 1);
-	
+
 	if (a == 0)
 		return (NULL);
-	
+
 	else
 	{
-		while (i < size)
+		for (; i < size; i++)
 			a[i] = str[i];
 	}
 
