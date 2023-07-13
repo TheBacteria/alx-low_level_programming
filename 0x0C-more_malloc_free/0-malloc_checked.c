729ae2364b2412a34c0af3591699866f25cf3d40
Bacteria <mahmoud1.zouine2@gmail.com>
2023-07-13 14:27:32 -0700

    C language

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocates memory
+ *
+ * @b: memory size
+ *
+ * Return: pointer to the memory reserved or NULL
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *a = malloc(b)
+
+	if (m == 0)
+		exit(98);
+	return (a);
+}
