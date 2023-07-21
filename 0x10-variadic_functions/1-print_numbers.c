d3fdda46a0287ceded5d9739e4c18677ad820ccd
Bacteria <mahmoud1.zouine2@gmail.com>
2023-07-20 17:12:05 -0700

    C language

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,26 +1,24 @@
-#include "variadic_function.h"
+#include "variadic_functions.h"
+
 /**
- * print_numbers - a functions that prints numbers with separator
+ * print_numbers - a function that prints numbers with separator
  *
  * @separator: the string separator
- * @n: number of arguments
- * @...: ints to print
- *
- * Return: void
+ * @n: the number of arguments
+ * @...: the ints to print
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int i = n;
-	va_list ap;
+	va_list args;
+	unsigned int i;
 
-	if (!n)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		printf("\n");
-		return;
+		printf("%d", va_arg(args, int));
+		if (i < n - 1 && separator)
+			printf("%s", separator);
 	}
-	va_start(ap, n);
-	while (i--)
-		printf("%d%s", va_arg(ap, int),
-				i ? (separator ? separator : "") : "\n");
-	va_end(ap);
+	va_end(args);
+	printf("\n");
 }
