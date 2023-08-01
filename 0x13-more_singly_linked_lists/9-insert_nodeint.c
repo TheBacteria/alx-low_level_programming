08d4a949bfc4284e9cdad91850ada5db2f2b84e2
TheBacteria <mahmoud1.zouine2@gmail.com>
2023-08-01 16:25:29 -0700

    C language

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at a given position
+ * insert_nodeint_at_index - inserts a new node at a given position
  *
  * @head: pointer to the first node
  * @idx: index to insert new node
